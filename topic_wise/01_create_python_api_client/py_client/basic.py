0f827ea9eba58ac05f61fd8dd8df33e67ce12495
Roman-Ojha <razzroman98@gmail.com>
2022-07-25 23:42:34 +0545

    docs: request to django server

--- topic_wise/01_create_python_api_client/py_client/basic.py
+++ topic_wise/01_create_python_api_client/py_client/basic.py
@@ -14,4 +14,8 @@ print(get_response.text)  # return in JSON Formate
 print(get_response.json())  # return python dictionary
 # return status code for that specific request
 print(get_response.status_code)
-print(requests.get(endpoint, json={"query": "Hello world"}).json())
+# print(requests.get(endpoint, json={"query": "Hello world"}).json())
+
+# requesting django endpoint
+django_endpoint = "http://localhost:8002/"
+print(requests.get(django_endpoint).status_code)
