c2b49afa5459c6272d218f5d714f6403a6e4bda6
Roman-Ojha <razzroman98@gmail.com>
2022-07-25 23:30:50 +0545

    docs: create python api client

---
+++ topic_wise/01_create_python_api_client/py_client/basic.py
@@ -0,0 +1,17 @@
+import requests
+
+# different endpoint that will response base some data
+endpoint = "http://httpbin.org/"
+endpoint = "http://httpbin.org/anything"
+
+# now we will use requests package to fetch the data from the endpoint
+# REST APIs -> Web based API
+# so it need http request
+get_response = requests.get(endpoint)
+# HTTP Request -(give)> HTML
+# Rest API HTTP request -(give)> JSON | XML
+print(get_response.text)  # return in JSON Formate
+print(get_response.json())  # return python dictionary
+# return status code for that specific request
+print(get_response.status_code)
+print(requests.get(endpoint, json={"query": "Hello world"}).json())
