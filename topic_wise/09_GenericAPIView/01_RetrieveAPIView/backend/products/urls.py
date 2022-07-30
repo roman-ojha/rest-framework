248ac51edd23d4342e0a17343472ef7bf076a63c
Roman-Ojha <razzroman98@gmail.com>
2022-07-30 22:46:09 +0545

    feat: retrieved generic model data

--- topic_wise/09_GenericAPIView/01_RetrieveAPIView/backend/products/urls.py
+++ topic_wise/09_GenericAPIView/01_RetrieveAPIView/backend/products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>', views.ProductDetailAPIView.as_view())
+    path('<int:pk>/', views.ProductDetailAPIView.as_view())
     # pk (primary key) which is available by default inside model
     # because we have use class based view 'ProductDetailAPIView' we have to use it as view
 ]
