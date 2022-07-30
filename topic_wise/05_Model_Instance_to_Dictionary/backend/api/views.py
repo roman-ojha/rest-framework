92c66ceb1da43561ee01d5b429ef3a68cfd94ecc
Roman-Ojha <razzroman98@gmail.com>
2022-07-30 12:28:27 +0545

    docs: converted model instance to dictionary

--- topic_wise/05_Model_Instance_to_Dictionary/backend/api/views.py
+++ topic_wise/05_Model_Instance_to_Dictionary/backend/api/views.py
@@ -1,18 +1,25 @@
-from django.http import JsonResponse
+from wsgiref import headers
+from django.http import JsonResponse, HttpResponse
+# JsonResponse: it accept a dictionary
+# HttpResponse: it accept a string
+
 import json
+
+from django.forms.models import model_to_dict
+# this is used in django form
+
 from products.models import Product
 
 
 def api_home(request, *args, **kwargs):
-    # now here we will get the render data from the database
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        # if model_data exist
-        # add data to dict
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
-    #  return JSON to client
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+        # now we don't have to manually create dictionary the way that we did before
+        # we can also declare the field that we want to add inside the dictionary
     return JsonResponse(data)
+    # return HttpResponse(json.dumps(data), headers={"content-type": "application/json"})
+    # because from HttpResponse we can only response string and we can't response dictionary after adding header as 'application/json'
+    # we can convert dictionary into json string
+    # But because we have to 'price' field which is in decimal and django can't Json Serialize Decimal value so rather on these case 'JsonResponse' handle dictionary pretty well
