f457bd1d695b0fc4deb9724b258c124c29ddf79e
Roman-Ojha <razzroman98@gmail.com>
2022-07-31 09:05:31 +0545

    feat: added perform create on CreateAPIView

--- topic_wise/09_GenericAPIView/02_CreateAPIView/backend/products/views.py
+++ topic_wise/09_GenericAPIView/02_CreateAPIView/backend/products/views.py
@@ -8,6 +8,19 @@ class ProductCreateAPIView(generics.CreateAPIView):
     serializer_class = ProductSerializer
     # As we can see that both the 'ProductCreateAPIView' & 'ProductDetailAPIView' looks same but there generic is different means that one use 'RetrieveAPIView' to get the mentioned queryset and one is used to create data and save it into database
 
+    # while create using this view we want to assign something then
+    def perform_create(self, serializer):
+        # this method can be called inside CreateAPIView
+
+        # to get the data that is been validated
+        print(serializer.validated_data)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            # if client did not send by client then we will save content value as title
+            content = title
+        serializer.save(content=content)
+
 
 product_create_view = ProductCreateAPIView.as_view()
 
