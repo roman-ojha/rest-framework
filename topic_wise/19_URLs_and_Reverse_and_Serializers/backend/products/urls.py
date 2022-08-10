72cc26c192af29803e2c3a4d15a571109306c954
Roman-Ojha <razzroman98@gmail.com>
2022-08-10 15:04:57 +0545

    feat: added URLs, reverse & Serializsers

--- topic_wise/19_URLs_and_Reverse_and_Serializers/backend/products/urls.py
+++ topic_wise/19_URLs_and_Reverse_and_Serializers/backend/products/urls.py
@@ -3,13 +3,13 @@ from . import views
 
 urlpatterns = [
     # /api/products/1/
-    path('<int:pk>/', views.product_detail_view),
+    path('<int:pk>/', views.product_detail_view, name='product-detail'),
     # /api/products/
     path('', views.product_create_view),
     # /api/products/list
-    path('list/', views.product_list_create_view),
+    path('list/', views.product_list_create_view, name='product-list'),
     # /api/products/1/update
-    path('<int:pk>/update/', views.product_update_view),
+    path('<int:pk>/update/', views.product_update_view, name='product-edit'),
     # /api/products/1/destroy
     path('<int:pk>/delete/', views.product_destroy_view),
 ]
