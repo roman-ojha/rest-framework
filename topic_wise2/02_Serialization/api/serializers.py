21ac7ebc6282223b9c699ae0c111969ae26c31e7
Roman-Ojha <razzroman98@gmail.com>
2022-12-25 13:48:25 +0545

    feat: serialized Model instance and response Json data into client

---
+++ topic_wise2/02_Serialization/api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from django.db import models
+
+
+# Creating Student model Serializer that will convert complex model object into python dict
+class StudentSerializer(serializers.Serializer):
+    # we need to provide the field and it's type to get serialized
+    name = serializers.CharField(max_length=100)
+    roll = serializers.IntegerField()
+    city = serializers.CharField(max_length=100)
--- topic_wise2/02_Serialization/api/views.py
+++ topic_wise2/02_Serialization/api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
 
-# Create your views here.
+# Model Object -> Single Student Data
+
+
+def student_detail(request):
+    # First getting Model object instance
+    stu = Student.objects.get(id=1)
+    # Serializing model object instance and converting into dict
+    serialized = StudentSerializer(stu)
+    print(serialized.data)
+    # Converting serialized data into JSON
+    json_data = JSONRenderer().render(data=serialized.data)
+    print(json_data)
+
+    # Returning json data as response to the client
+    return HttpResponse(json_data, content_type='application/json')
