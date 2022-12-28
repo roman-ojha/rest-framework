9713c469e7d464573b80c501b977cab64fc5814b
Roman-Ojha <razzroman98@gmail.com>
2022-12-28 18:13:24 +0545

    feat: implemented CRUD operation using GenericAPIView & Mixins

--- topic_wise2/10_GenericAPIView_and_Mixins/api/views.py
+++ topic_wise2/10_GenericAPIView_and_Mixins/api/views.py
@@ -4,7 +4,7 @@ from .models import Student
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import ListModelMixin, CreateModelMixin
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 
 # CRUD Operation Using GenericAPIView & Model Mixin:
 
@@ -27,59 +27,74 @@ class StudentList(GenericAPIView, ListModelMixin):
 # This view will Create the 'Student' data and save into database
 # Implement POST Method
 class StudentCreate(GenericAPIView, CreateModelMixin):
-    # For that we have to provide the queryset that it perform to response list of record
     queryset = Student.objects.all()
-
-    # also we have to provide the serializer class for serializing the data
     serializer_class = StudentSerializer
 
     # CreateModelMixin implement the post method that will create a new object and save into database
     def post(self, request, *args, **kwargs):
-        # by returning list method now we can get all the list of data from 'Student' table
         return self.create(request, *args, **kwargs)
 
 
-# Class Based APIView
-class StudentAPI(APIView):
-    def get(self, request, pk=None, format=None):
-        id = pk
-        if id is not None:
-            student = Student.objects.get(id=id)
-            serializer = StudentSerializer(student)
-            return Response(serializer.data)
-        students = Student.objects.all()
-        serializer = StudentSerializer(students, many=True)
-        return Response(serializer.data)
+# This view will response the single object from the record
+# Implement GET Method
+# Required 'pk' from dynamic url
+class StudentRetrieve(GenericAPIView, RetrieveModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
 
-    def post(self, request, format=None):
-        data = request.data
-        serializer = StudentSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'msg': 'Data Created'}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # RetrieveModelMixin implement the get method that will response a single object from the record
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, pk, format=None):
-        id = pk
-        student = Student.objects.get(pk=id)
-        serializer = StudentSerializer(
-            student, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"msg": "Data Updated"})
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def patch(self, request, pk, format=None):
-        id = pk
-        student = Student.objects.get(pk=id)
-        serializer = StudentSerializer(
-            student, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"msg": "Data Updated"})
-        return Response(serializer.errors)
+# This view will update the record in the database
+# Implement PUT & PATCH Method
+class StudentUpdate(GenericAPIView, UpdateModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
 
-    def delete(self, request, pk, format=None):
-        id = pk
-        Student.objects.get(id=id).delete()
-        return Response({'msg': "Data deleted"})
+    # UpdateModelMixin implement the put method that will update the record on database
+    def put(self, request, *args, **kwargs):
+        # this will work as Put as well as Patch
+        return self.update(request, *args, **kwargs)
+
+
+# This view will delete the record from the database
+# Implement DELETE Method
+class StudentDestroy(GenericAPIView, DestroyModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    # DestroyModelMixin implement the delete method that will delete the record from the database
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+# CRUD Operation using all the Mixins
+# On 'ListModelMixin' & 'CreateModelMixin' doesn't required 'pk'
+# On 'RetrieveModelMixin', 'UpdateModelMixin' & 'DestroyModelMixin' does required 'pk'
+# So these two are the Group so we will create different View class for this
+class StudentLC(GenericAPIView, ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+# On 'RetrieveModelMixin', 'UpdateModelMixin' & 'DestroyModelMixin' does required 'pk'
+class StudentRUD(GenericAPIView, ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
--- topic_wise2/10_GenericAPIView_and_Mixins/config/urls.py
+++ topic_wise2/10_GenericAPIView_and_Mixins/config/urls.py
@@ -5,6 +5,9 @@ from api import views
 urlpatterns = [
     path('list/', views.StudentList.as_view()),
     path('create/', views.StudentCreate.as_view()),
-    path('crud/', views.StudentAPI.as_view()),
-    path('crud/<int:pk>', views.StudentAPI.as_view())
+    path('retrieve/<int:pk>/', views.StudentRetrieve.as_view()),
+    path('update/<int:pk>/', views.StudentUpdate.as_view()),
+    path('destroy/<int:pk>/', views.StudentDestroy.as_view()),
+    path('lc/', views.StudentLC.as_view()),
+    path('rud/<int:pk>/', views.StudentRUD.as_view()),
 ]
