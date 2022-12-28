73221ec168dcdb0dfbb87ba61ff5621a3e316325
Roman-Ojha <razzroman98@gmail.com>
2022-12-28 14:51:48 +0545

    feat: create example of DRF function based api_view for GET & POST

--- topic_wise2/08_Function_Based_API_View/api/views.py
+++ topic_wise2/08_Function_Based_API_View/api/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+# NOTE: if you use 'api_view' decorators in that case you will get Browsable API on you Browser
+
+# using DRF 'api_view' to create view
+# @api_view()  # default GET request is accepted
+
+
+@api_view(['GET'])  # Or you can provide the GET method both are same
+def hello_world(request):
+    return Response({'msg': 'Hello world'})
+
+
+# POST Method view
+@api_view(['POST'])
+def h_w(request):
+    if request.method == "POST":
+        # get data passed by requested user
+        data = request.data
+        print(data)
+        return Response({'msg': 'This is post request'})
+
+
+# GET & POST Method view
+@api_view(['GET', 'POST'])
+def h_w_gp(request):
+    if request.method == "GET":
+        return Response({'msg': 'This is get request'})
+    elif request.method == "POST":
+        # get data passed by requested user
+        data = request.data
+        return Response({'msg': 'This is post request', 'data': data})
---
+++ topic_wise2/08_Function_Based_API_View/client/app.py
@@ -0,0 +1,122 @@
+import requests
+import json
+
+
+def hello_world(url="http://127.0.0.1:8000/hw/"):
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = requests.get(url=url, headers=headers)
+    data = response.json()
+    print(data)
+
+
+# hello_world()
+
+
+def h_w_post(url="http://127.0.0.1:8000/hwp/"):
+    data = {
+        'name': 'Roman'
+    }
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    json_data = json.dumps(data)
+    response = requests.post(
+        url=url, data=json_data, headers=headers)
+    res_data = response.json()
+    print(res_data)
+
+
+# h_w_post()
+
+
+def h_w_gp():
+    hello_world("http://127.0.0.1:8000/hwgp/")
+    h_w_post("http://127.0.0.1:8000/hwgp/")
+
+
+h_w_gp()
+
+
+# For Function based View
+# URL = "http://127.0.0.1:8000/crud/"
+
+def read(id=None):
+    data = {}
+    if id is not None:
+        data = {'id': id}
+
+    # converting python dict into JSON
+    json_data = json.dumps(data)
+
+    # requesting
+    response = requests.get(url=URL, data=json_data)
+    res_data = response.json()
+    print(res_data)
+
+
+# read()
+# read(5)
+
+
+def insert():
+    data = {
+        'name': 'Roman',
+        'roll': 35,
+        'city': 'Kathmandu'
+    }
+    json_data = json.dumps(data)
+    response = requests.post(url=URL, data=json_data)
+    res_data = response.json()
+    print(res_data)
+
+
+# insert()
+
+
+def update():
+    def partial():
+        data = {
+            # passing partial update data
+            'id': 5,
+            'name': 'Harry',
+            'city': 'NewYork'
+        }
+        json_data = json.dumps(data)
+        response = requests.put(url=URL, data=json_data)
+        res_data = response.json()
+        print(res_data)
+
+    # partial()
+
+    def complete():
+        data = {
+            # passing complete update data
+            'id': 6,
+            'name': 'Razz',
+            'roll': 30,
+            'city': 'Pokhara'
+        }
+        json_data = json.dumps(data)
+        response = requests.patch(url=URL, data=json_data)
+        res_data = response.json()
+        print(res_data)
+
+    complete()
+
+
+# update()
+
+
+def delete():
+    data = {
+        'id': 5,
+    }
+    json_data = json.dumps(data)
+    response = requests.delete(url=URL, data=json_data)
+    res_data = response.json()
+    print(res_data)
+
+
+# delete()
