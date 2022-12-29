2f1af2a191c0883fe1bccc9a057a7a75c41c669f
Roman-Ojha <razzroman98@gmail.com>
2022-12-29 13:26:58 +0545

    feat: created custom class to generate token

--- topic_wise2/18_TokenAuthentication/api/views.py
+++ topic_wise2/18_TokenAuthentication/api/views.py
@@ -4,6 +4,26 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
+"""
+Generate token through:
+1. http://127.0.0.1:8000/adminauthtoken/tokenproxy/add/
+2. Command: python manage.py drf_create_token <username>
+3. Using API endpoints: create endpoint inside ./urls.py using 'obtain_auth_token' function based view
+    => EX: we can use command line to access the api endpoint
+        -> install 'httpie' package
+        -> pip install httpie
+        -> Command: http POST http://127.0.0.1:8000/gettoken/ username="" password= "password"
+        -> Response(default): {'token': 'd8b1ad0a0a28d062b9e9ad2e6c76a0cd00155ade'}
+
+    => You can manage the response body as well as other thing for 'obtain_auth_token' api view by creating custom auth token
+        -> we will create this inside './auth.py'
+        -> and we will use that custom class on './urls.py'
+"""
+
+
+# Get generated token:
+# Command: python manage.py drf_create_token <generated_user_token_username>
+
 
 class StudentModelViewSet1(viewsets.ModelViewSet):
     queryset = Student.objects.all()
--- topic_wise2/18_TokenAuthentication/config/urls.py
+++ topic_wise2/18_TokenAuthentication/config/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from api import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+from api.auth import CustomAuthToken
 
 router = DefaultRouter()
 
@@ -10,7 +12,12 @@ router.register('studentapi', views.StudentModelViewSet1, basename='student')
 urlpatterns = [
     path('admin', admin.site.urls),
     path('', include(router.urls)),
-    # To authenticated to user for 'SessionAuthentication' DRF provide the url where we can go and authenticated ourself
-    path('auth/', include('rest_framework.urls'), name='rest_framework')
-    # after adding this we can see the 'Login' button on DRF Browsable API
+    path('auth/', include('rest_framework.urls'), name='rest_framework'),
+    # To get or create token for user we have to use 'obtain_auth_token' view created by DRF
+    # which accept 'username' & 'password' as credential
+    path('gettoken/', obtain_auth_token),
+    path('gettoken/', obtain_auth_token),
+
+    # Using Custom class to object token
+    path('gettoken2/', CustomAuthToken.as_view())
 ]
