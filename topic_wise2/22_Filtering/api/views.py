299c367ce9a0d3135b7e11ecbfc9b3d1b6e89eeb
Roman-Ojha <razzroman98@gmail.com>
2022-12-30 10:54:22 +0545

    feat(filtering): implemented filtering using get_queryset() & docs realted to DjangoFilterBackend

--- topic_wise2/22_Filtering/api/views.py
+++ topic_wise2/22_Filtering/api/views.py
@@ -6,5 +6,18 @@ from .models import Student
 
 # Create your views here.
 class StudentList(ListAPIView):
+    # queryset = Student.objects.all()
+
+    # if you want to filter by 'passby' field get the Student 'passby' xxx in that case we can change the query set like this:
+    # queryset = Student.objects.filter(passby='roman')
+    # But we will set the query set as 'all()'
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+
+    # and we will filter the queryset with those user that get logged in
+    # so if 'roman' user is authenticated and try to call this api in that case we want to filter the student 'passby' 'roman' user
+    # for that we have to override the 'get_queryset' method
+    def get_queryset(self):
+        user = self.request.user
+        print(user)
+        return Student.objects.filter(passby=user)
--- topic_wise2/22_Filtering/config/urls.py
+++ topic_wise2/22_Filtering/config/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('list/', views.StudentList.as_view()),
+    path('auth/', include('rest_framework.urls')),
 ]
