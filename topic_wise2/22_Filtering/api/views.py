88428e386b5e721de07b72658ab00446543b9c8d
Roman-Ojha <razzroman98@gmail.com>
2022-12-30 10:35:55 +0545

    feat(filtering): created Student Model, serializer, view class

--- topic_wise2/22_Filtering/api/views.py
+++ topic_wise2/22_Filtering/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .serializsers import StudentSerializer
+from .models import Student
+
 
 # Create your views here.
+class StudentList(ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
