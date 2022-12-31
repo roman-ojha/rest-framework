65cfdbdc4f0bd7318bba414187c3b227971a11db
Roman-Ojha <razzroman98@gmail.com>
2022-12-31 17:28:52 +0545

    feat: implemented PageNumberPagination

--- topic_wise2/25_Page_Number_Pagination/api/views.py
+++ topic_wise2/25_Page_Number_Pagination/api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .serializers import StudentSerializer
+from .models import Student
+from rest_framework.generics import ListAPIView
+from .pagination import MyPageNumberPagination
+
 
 # Create your views here.
+class StudentList(ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    # If you want to add PageNumberPagination globally in that case you have to add this inside 'settings.py' file
+    # REST_FRAMEWORK = {
+    #     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    #     'PAGE_SIZE': 5
+    # }
+
+    # If you want to add Pagination locally in that case you can add like this:
+    # pagination_class = PageNumberPagination
+    # But just by adding 'PageNumberPagination' on 'pagination_class' it won't work because we have not specify the 'page_size' so for that we can inherit the 'PageNumberPagination' and create a new class then we can add that class here
+
+    # using the pagination that we created
+    pagination_class = MyPageNumberPagination
