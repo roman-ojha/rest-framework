55e13d207213c6ec7aadf0a0e461531232fb0a06
Roman-Ojha <razzroman98@gmail.com>
2023-01-02 12:21:50 +0545

    feat: implemented Nested Serializer

--- topic_wise2/30_Nested_Serializer/api/serializers.py
+++ topic_wise2/30_Nested_Serializer/api/serializers.py
@@ -3,25 +3,16 @@ from rest_framework import serializers
 
 
 class SongSerializer(serializers.ModelSerializer):
-    singer = serializers.StringRelatedField(read_only=True)
-
     class Meta:
         model = Song
         fields = ['id', 'title', 'singer', 'duration']
 
 
 class SingerSerializer(serializers.ModelSerializer):
-    song = serializers.StringRelatedField(many=True, read_only=True)
-    song = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-
-    song = serializers.HyperlinkedRelatedField(
-        many=True, read_only=True, view_name='song-detail')
-    song = serializers.SlugRelatedField(
-        many=True, read_only=True, slug_field='title')
-    song = serializers.SlugRelatedField(
-        many=True, read_only=True, slug_field='duration')
-    song = serializers.HyperlinkedIdentityField(
-        many=True, read_only=True, view_name='song-detail')
+    # Using 'SongSerializer' and adding field to get the song sang by the singer
+    # Nested Serializer
+    song = SongSerializer(many=True, read_only=True)
+    # NOTE: 'song' is the related_name specify inside 'Song' model 'singer' field
 
     class Meta:
         model = Singer
