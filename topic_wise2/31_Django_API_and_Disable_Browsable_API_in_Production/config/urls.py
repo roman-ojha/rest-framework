0e6a7134567a13584c7e17ba9fb4022ad9e37933
Roman-Ojha <razzroman98@gmail.com>
2023-05-05 15:18:15 +0545

    feat: created Django api from already create django project

--- topic_wise2/31_Django_API_and_Disable_Browsable_API_in_Production/config/urls.py
+++ topic_wise2/31_Django_API_and_Disable_Browsable_API_in_Production/config/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from enroll import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.add_and_show, name="add_and_show"),
     path('delete/<int:id>/', views.delete_student, name="delete_student"),
-    path('update/<int:id>/', views.update_student, name="update_student")
+    path('update/<int:id>/', views.update_student, name="update_student"),
+    path('api/',include('enroll.api.urls'))
 ]
---
+++ topic_wise2/31_Django_API_and_Disable_Browsable_API_in_Production/enroll/api/views.py
@@ -0,0 +1,11 @@
+from enroll.models import User
+from enroll.api.serializer import UserSerializer
+from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
